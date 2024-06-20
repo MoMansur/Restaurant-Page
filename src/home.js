f24643dff06a74743f6581989ec42e6cfa7c613c
MoMansur <mansurshehumsm@gmail.com>
2024-06-20 23:01:57 +0300

    Images added, Pages and config

---
+++ src/home.js
@@ -0,0 +1,10 @@
+import logo from './img/logo.png'
+
+export default function homePageDom(){
+    const span = document.createElement('span')
+
+    span.style.backgroundColor = 'green'
+    
+    document.body.append(span)
+
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-alert('Hello')
+
+import './style.css';
+
+function component() {
+    const element = document.createElement('div');
+ 
+    
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.classList.add('hello');
+ 
+
+     // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+  
+    return element;
+  }
+ 
+  document.body.appendChild(component());
