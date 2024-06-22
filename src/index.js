9494fe81a68d708b82686ed02ec1831b35629dce
MoMansur <mansurshehumsm@gmail.com>
2024-06-22 23:36:16 +0300

    header logo added

--- src/index.js
+++ src/index.js
@@ -3,16 +3,29 @@ import menuPage from './menu.js';
 import homePageDom from './home.js';
 import aboutUsPageDom from './about.js';
 
+import logo from './img/Dlogo.png'
+
 
 function component() {
 const header = document.createElement('header');
 const nav = document.createElement('nav');
+
+// Create logo image element
+const logoImg = document.createElement('img');
+logoImg.src = logo; // Replace with actual path to your logo image
+logoImg.alt = 'Zulu Restaurant Logo'; // Alt text for accessibility
+logoImg.className = 'logo'; // Optional: Add a class for styling
+
+// Append logo image to header
+header.appendChild(logoImg);
 
 // Create buttons and append to nav
 const homeButton = document.createElement('button');
 homeButton.className = 'btn';
 homeButton.textContent = 'Home';
 nav.appendChild(homeButton);
+
+
 
 const menuButton = document.createElement('button');
 menuButton.className = 'btn';
@@ -68,7 +81,25 @@ aboutButton.addEventListener('click', ()=>{
   aboutUsPageDom(Container)
 })
   
-    return contentDiv;
+const footer = document.createElement('footer');
+
+const copyrightText = document.createTextNode('\u00A9 2024 by Mo_Mansur ');
+
+const githubLink = document.createElement('a');
+githubLink.href = 'https://github.com/MoMansur';
+githubLink.classList.add('link-offset-2', 'link-offset-3-hover', 'link-underline', 'link-underline-opacity-0', 'link-underline-opacity-75-hover');
+
+
+const githubText = document.createTextNode(' Mo_Mansur GitHub');
+githubLink.appendChild(githubText);
+
+const rightsReservedText = document.createTextNode('. All rights reserved.');
+
+footer.appendChild(copyrightText);
+footer.appendChild(githubLink);
+footer.appendChild(rightsReservedText);
+
+contentDiv.append(footer)
   }
  
   document.body.appendChild(component());
